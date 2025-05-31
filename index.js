0e016e3f0b5eb8c0b4b2e55d8e4a1e794800be37
Mark Wells <61658816+john-wells-ai1@users.noreply.github.com>
2025-05-30 20:11:37 -0700

    Update index.js - remove 'exports' from module script

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import OpenAI from 'openai';
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-exports.chat = async (req, res) => {
+export async chat(req, res) => {
   const assistantId = process.env.ASSISTANT_ID;
   const { threadId, message } = req.body;
 
